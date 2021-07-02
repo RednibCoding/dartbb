fe434382aac98b8a324aacbdd9c8a75f00793bb2
Michael Binder <rednib.coding@gmail.com>
2021-07-02 20:04:46 +0200

    Removed unused statements

--- web/main.dart
+++ web/main.dart
@@ -13,6 +13,11 @@ void main() async {
   ScaleImage(image1, 2, 2);
   SetImageMidhandle(image1, true);
 
+  // A copy basically copies the image element but not the attributes like scale/rotation/position etc.
+  // As you can see above, 'image1' has been scaled and the pivot point has been changed, but that does not
+  // apply to the copies when they are copied with 'CopyImage'
+  // This is usefull when you don't want to load the same image media multiple times (in this case 'media/darticon.png').
+  // To make an exact copy with all attributes, use CloneImage(Image) instead.
   image2 = CopyImage(image1);
   image3 = CopyImage(image1);
   image4 = CopyImage(image1);
@@ -23,9 +28,11 @@ void main() async {
   SetFont(font, 12);
 
   // Begin the render loop and therefore the given "mainLoop" (this must be called after all resources have been loaded)
+  // This should be the last command as this results in an endless loop
   RunGame();
 }
 
+// Create some variables for the images and font
 late Image image1;
 late Image image2;
 late Image image3;
@@ -33,9 +40,10 @@ late Image image4;
 late Image image5;
 late Image image6;
 late Font font;
-num rot = 2;
 
+// The mainLoop that has been given to the Graphics command
 void mainLoop() {
+  // Clear the screen
   Cls();
 
   var mhLeft = MouseHit(0);
@@ -75,13 +83,11 @@ void mainLoop() {
         MouseX() - 50, MouseY() + 80);
   }
 
-  // Reverse rotation on mouse click
-  if (mhLeft) {
-    rot /= -1;
-  }
-
   var anchorX = 120;
   var anchorY = 100;
+
+  // Auto animating is pretty easy right? :)
+
   AutoScaleImageX(image2, 0.04, 0, 1, true);
   DrawImage(image2, anchorX, anchorY);
 
